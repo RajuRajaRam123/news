e6f301559c7a6fcaccb7a5a940a60628b1eacfc1
unknown <pandeyanil2782@gmail.com>
2021-01-18 19:56:08 +0545

    index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP PRACTICE </title>
+</head>
+<body>
+ <div> this is my first php.</div>
+ <?php
+ 	$clz="Bernhardt";$place="bafal";
+
+ 	echo"this is my php"."<br>";
+ 	echo "My name is anil pandey.I study in". $clz."which is located at ".$place;
+ ?>
+</body>
+</html>
